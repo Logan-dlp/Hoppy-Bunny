3afc702445c108ac50b0e068f14eb3dd0633b028
Logan <pix.59700@gmail.com>
2024-06-10 20:29:40 +0200

    Adding Woodplane

--- Assets/_Core/Scripts/DynamicEnvironment/WoodPlane.cs
+++ Assets/_Core/Scripts/DynamicEnvironment/WoodPlane.cs
@@ -9,13 +9,20 @@ namespace DynamicEnvironment
         [SerializeField] private Transform _unspawnPoint;
         [SerializeField] private float _speed;
 
+        private Vector3 _initialPosition;
+
+        private void Awake()
+        {
+            _initialPosition = transform.position;
+        }
+
         private void Update()
         {
             transform.position += transform.forward * _speed * Time.deltaTime;
 
             if (transform.position.x - _unspawnPoint.position.x < .5f)
             {
-                transform.position = _spawnPoint.position;
+                transform.position = _initialPosition;
             }
         }
     }
