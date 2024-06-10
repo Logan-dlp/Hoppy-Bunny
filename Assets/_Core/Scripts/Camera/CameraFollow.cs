f198f6d09253e80622bed47720f8040eacf728a8
Logan <pix.59700@gmail.com>
2024-06-10 20:46:28 +0200

    Smooted Camera

---
+++ Assets/_Core/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _smoothSpeed = 1;
+        [SerializeField] private Vector3 _offset;
+
+        private void Awake()
+        {
+            transform.position = _target.position + _offset;
+            transform.LookAt(_target);
+        }
+
+        private void Update()
+        {
+            Vector3 desiredPosition = _target.position + _offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
