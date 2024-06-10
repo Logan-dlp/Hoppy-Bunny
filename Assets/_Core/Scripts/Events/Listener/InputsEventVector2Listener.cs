726f03b55103fa8c0ff2647702106230aa61691d
Logan <pix.59700@gmail.com>
2024-06-10 14:41:08 +0200

    Bunny movement
    Input use event

---
+++ Assets/_Core/Scripts/Events/Listener/InputsEventVector2Listener.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Events
+{
+    namespace Listener
+    {
+        public class InputsEventVector2Listener : MonoBehaviour
+        {
+            [SerializeField] private InputsEventVector2 _inputsEventVector2;
+            [SerializeField] private UnityEvent<Vector2> _callbacks;
+
+            private void OnEnable()
+            {
+                _inputsEventVector2.Vector2Action += InvokeEvent;
+            }
+
+            private void OnDisable()
+            {
+                _inputsEventVector2.Vector2Action -= InvokeEvent;
+            }
+
+            private void InvokeEvent(Vector2 vector2)
+            {
+                _callbacks?.Invoke(vector2);
+            }
+        }
+    }
+}
