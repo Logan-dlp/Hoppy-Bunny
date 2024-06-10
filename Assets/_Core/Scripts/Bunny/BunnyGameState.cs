05bc2973ac17af17d8fb2303ab20b7462681d9b1
Logan <pix.59700@gmail.com>
2024-06-10 15:21:37 +0200

    Car Movement & detect player

---
+++ Assets/_Core/Scripts/Bunny/BunnyGameState.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Bunny
+{
+    public class BunnyGameState : MonoBehaviour
+    {
+        public void Death()
+        {
+            Debug.Log("You are death");
+        }
+    }
+}
---
+++ Assets/_Core/Scripts/DynamicEnvironment/Car.cs
@@ -0,0 +1,31 @@
+using System;
+using Bunny;
+using UnityEngine;
+
+namespace DynamicEnvironment
+{
+    public class Car : MonoBehaviour
+    {
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private Transform _unspawnPoint;
+        [SerializeField] private float _speed;
+
+        private void FixedUpdate()
+        {
+            transform.position += transform.forward * _speed * Time.fixedDeltaTime;
+
+            if (Vector3.Distance(transform.position, _unspawnPoint.position) < .1f)
+            {
+                transform.position = _spawnPoint.position;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out BunnyGameState bunnyGameState))
+            {
+                bunnyGameState.Death();
+            }
+        }
+    }
+}
